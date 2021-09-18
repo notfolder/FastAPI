30cabf0a341b887b1a7d9109450fb20976400a64
notfolder <notfolder@gmail.com>
2021-09-18 21:25:19 +0900

    log追加

--- main.py
+++ main.py
@@ -1,25 +1,58 @@
-from typing import List
-from fastapi import FastAPI
-import uvicorn
-import uuid
 import asyncio
-from pydantic import BaseModel
 import time
+import uuid
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
+from logging import config
+from typing import List
+from multiprocessing import Pool
+import logging
+
+import uvicorn
+import yaml
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+config.dictConfig(yaml.load(open("logging.yml").read(),
+                            Loader=yaml.SafeLoader))
+
 queue: List[dict] = []
+
+log = logging.getLogger('process')
 
 
 class Item(BaseModel):
     name: str
 
 
-def process_f(id: int):
-    print(id)
-    time.sleep(5)
+def getLogger(id: str):
+    log = logging.getLogger(f'process.{id}')
+    handler = logging.FileHandler(f'{id}.log')
+    fmt = yaml.load(
+        open("logging.yml").read(),
+        Loader=yaml.SafeLoader)['formatters']['simple_fmt']['format']
+    handler.setFormatter(logging.Formatter(fmt))
+    log.addHandler(handler)
+    return log
+
+
+def sub_proccess_f(id: str, n: int):
+    log = getLogger(id)
+    log.info(f'start: {id}-{n}')
+    time.sleep(2)
+    log.info(f'end: {id}-{n}')
+
+
+def process_f(id: str):
+    log = getLogger(id)
+    log.info(f'start: {id}')
+    with Pool(2) as p:
+        log.info(
+            p.starmap(sub_proccess_f, [(id, 1), (id, 2), (id, 3), (id, 4)]))
+    #time.sleep(5)
+    log.info(f'end: {id}')
     return id
 
 
@@ -33,15 +66,11 @@ async def run_next_task():
         return
     task['status'] = 'processing'
     # main task
-    #with Pool(2) as p:
-    #    print(p.map(process_f, [1, 2, 3]))
 
     loop = asyncio.get_event_loop()
-    executor = ProcessPoolExecutor(max_workers=2)
-    result = await asyncio.gather(*[
-        loop.run_in_executor(executor, partial(process_f, 1)),
-        loop.run_in_executor(executor, partial(process_f, 2)),
-    ])
+    executor = ProcessPoolExecutor(max_workers=1)
+    result = await loop.run_in_executor(executor,
+                                        partial(process_f, task['id']))
     task['result'] = result
 
     #await asyncio.sleep(5)
@@ -78,7 +107,7 @@ def read_root():
 @app.post("/task")
 async def post_task(item: Item):
     id = str(uuid.uuid4())
-    print(id)
+    log.info(id)
     async_task = asyncio.get_event_loop().create_task(run_next_task())
     task = {
         "id": id,
