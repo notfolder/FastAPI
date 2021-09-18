69b291fc43d02ce63ab762c54140ec35d048cf32
notfolder <notfolder@gmail.com>
2021-09-18 23:28:26 +0900

    nginx docker追加

--- main_test.py
+++ main_test.py
@@ -3,33 +3,39 @@ import pytest
 from httpx import AsyncClient
 from multiprocessing import Process
 import uvicorn
+import time
 
 from main import app
 
 
-@pytest.mark.asyncio
-async def test_root():
+async def root_tester(app):
     async with AsyncClient(app=app, base_url="http://127.0.0.1:5000") as ac:
         response = await ac.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
 
 
-def run_server():
+@pytest.mark.asyncio
+async def test_root():
+    root_tester(app)
+
+
+async def run_server():
     uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")
 
 
 @pytest.fixture
-def server():
+async def server():
     proc = Process(target=run_server, args=(), daemon=True)
     proc.start()
     yield
-    proc.kill()  # Cleanup after test
+    proc.kill()
 
 
 @pytest.mark.asyncio
 async def test_root_server(server):
-    await test_root()
+    await asyncio.sleep(1)
+    await root_tester(None)
 
 
 @pytest.mark.asyncio
