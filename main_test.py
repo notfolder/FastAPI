bcaca9c8b7588fad0637bf70b3ff0b1d62b3c716
notfolder <notfolder@gmail.com>
2021-09-19 10:36:48 +0900

    何故かeventloopが回らない

--- main_test.py
+++ main_test.py
@@ -20,26 +20,26 @@ async def test_root():
     root_tester(app)
 
 
-async def run_server():
+def run_server():
     uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")
 
 
-@pytest.fixture
-async def server():
+@pytest.fixture()
+async def server(request):
     proc = Process(target=run_server, args=(), daemon=True)
     proc.start()
-    yield
-    proc.kill()
+    time.sleep(0.3)
+    #yield
+    #proc.kill()
+    request.addfinalizer(proc.kill)
 
 
 @pytest.mark.asyncio
 async def test_root_server(server):
-    await asyncio.sleep(1)
     await root_tester(None)
 
 
-@pytest.mark.asyncio
-async def test_task():
+async def task_tester(app):
     async with AsyncClient(app=app, base_url="http://127.0.0.1:5000") as ac:
         response = await ac.post("/task", json={"name": "task1"})
     assert response.status_code == 200
@@ -58,36 +58,13 @@ async def test_task():
 
 
 @pytest.mark.asyncio
-async def test_task_server(server):
-    await test_task()
+async def test_task():
+    await task_tester(app)
 
 
 @pytest.mark.asyncio
 async def test_task_server(server):
-    await test_task()
-
-
-# class RemoteTestApp(asynctest.TestCase):
-#     async def setUp(self):
-#         self.proc = Process(target=uvicorn.run,
-#                             args=(app.api,),
-#                             kwargs={
-#                                 "host": "127.0.0.1",
-#                                 "port": 5000,
-#                                 "log_level": "info"},
-#                             daemon=True)
-#         self.proc.start()
-#         await asyncio.sleep(0.1)  # time for the server to start
-
-#     async def tearDown(self):
-#         """ Shutdown the app. """
-#         self.proc.terminate()
-
-#     async def test_root(self):
-#         async with AsyncClient(app=app, base_url="http://127.0.0.1:5000") as ac:
-#             response = await ac.get("/")
-#         assert response.status_code == 200
-#         assert response.json() == {"Hello": "World"}
+    await task_tester(None)
 
 
 def main():
